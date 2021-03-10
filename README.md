2ea5e538e68bb32ee484703246927e44521ec03a
Tom Doughty <thomashdoughty@gmail.com>
2021-03-10 17:49:10 +0000

    Refactor for NPM

--- README.md
+++ README.md
@@ -1,45 +1,31 @@
-# NHS UK Banner
-![Master](https://github.com/tomdoughty/banner/workflows/Push%20to%20master/badge.svg?event=push)
+# NHS.UK emergency banner
 
-In house solution for managing the emergency banner and feedback banners on [nhs.uk](https://www.nhs.uk).
+![Master](https://github.com/nhsuk/banner/workflows/Push%20to%20master/badge.svg?event=push)
 
-Please note the nhsuk frontend team will be working towards better practices for consuming this code. In the future it should be consumed via NPM. Draft documentation on the NPM solution is in [WIP-README.md](WIP-README.md).
+In house solution for managing the emergency banner on [nhs.uk](https://www.nhs.uk).
 
 ## Usage
-
-### Compiled files
-Add the compiled CSS and JS files to your application.
+This code should be consumed via NPM. You should already be using NPM to consume the [nhsuk-frontend library](https://github.com/nhsuk/nhsuk-frontend) via a build tool such as Gulp or Webpack.
 ```
-dist/banner.css
-dist/banner.js
+npm install @nhsuk/emergency-banner --save
 ```
 
-### SCSS Imports
-If you already include nhsuk-frontend core SCSS then you only need to add the following files to add the styles to your CSS.
+### CSS
+The styles for the banner are no longer included in nhsuk-frontend. You will need import the SCSS from NPM.
 ```
-/src/scss/emergency-alert
-/src/scss/feedback-banner
+@import 'node_modules/@nhsuk/emergency-banner/dist/emergency-banner';
 ```
-This will prevent you from including duplicate CSS in your application.
 
-### No JavaScript solution
-The nhs.uk policy for JavaScript is progressive enhancement. As a non JavaScript solution we display a link to a content managed page which makes the alert API request server side.
-Include the following code below your header to include this functionality in your application.
+### JavaScript
+The JavaScript in this repository is written in ES6. For production use it must be transpiled to ES5. This process should be handled by your build tools.
 ```
-<noscript>
-  <div class="nhsuk-global-alert">
-    <div class="nhsuk-width-container">
-	  <a href="http://www.nhs.uk/pages/NoJsAandE.aspx">Check here for alerts</a>
-    </div>
-  </div>
-</noscript>
+import '@nhsuk/emergency-banner';
 ```
 
 ## Development
 
 ### Application
 ```
-git clone git@github.com:nhsuk/nhsuk-banner.git
 npm install
 npm start
 ```
--- tests/index.test.js
+++ tests/index.test.js
@@ -1,17 +1,10 @@
 import {
   getBannerApiUrl,
-  hideBanner,
-  checkUrlArray,
-  isMatchedUrl,
-  bannerIsValid,
   insertBanner,
 } from '../src/js';
 
-import { setCookie } from '../src/js/cookies';
 import alertBannerData from '../mocks/emergency-banner.json';
 import alertBannerMarkup from '../mocks/emergency-banner';
-import feedbackBannerData from '../mocks/feedback-banner.json';
-import feedbackBannerMarkup from '../mocks/feedback-banner';
 import coronavirusBannerData from '../mocks/coronavirus-banner.json';
 import coronavirusBannerMarkup from '../mocks/coronavirus-banner';
 
@@ -41,120 +34,6 @@ describe('getBannerApiUrl()', () => {
   });
 });
 
-describe('checkUrlArray()', () => {
-  const urls = 'https://test.api.co.uk;https://test.api.com;https://test.api.uk';
-
-  beforeEach(() => {
-    delete window.location;
-  });
-
-  it('returns true if the current url is in the urls string', () => {
-    window.location = { href: 'https://test.api.com' };
-    expect(checkUrlArray(urls)).toBe(true);
-  });
-
-  it('returns false if the current url is not in the urls string', () => {
-    window.location = { href: 'https://fake.api.com' };
-    expect(checkUrlArray(urls)).toBe(false);
-  });
-});
-
-describe('hideBanner()', () => {
-  beforeEach(() => {
-    document.body.innerHTML = '<div id="nhsuk-feedback-banner"></div>';
-  });
-
-  it('hides the survey banner using CSS', () => {
-    const banner = document.getElementById('nhsuk-feedback-banner');
-    expect(banner).toBeDefined();
-    hideBanner();
-    expect(banner.style.display).toBe('none');
-  });
-});
-
-describe('checkUrlArray()', () => {
-  const urls = 'https://test0.com/;https://test1.com/;https://test2.com/';
-
-  it('returns true if the current url is in the semi colon separated list of urls', () => {
-    window.location = { href: 'https://test1.com/' };
-    expect(checkUrlArray(urls)).toBe(true);
-  });
-
-  it('returns false if the current url is not in the semi colon separated list of urls', () => {
-    window.location = { href: 'https://test3.com/' };
-    expect(checkUrlArray(urls)).toBe(false);
-  });
-});
-
-describe('isMatchedUrl()', () => {
-  const excludedUrls = 'https://test0.com/;https://test1.com/';
-  const includedUrls = 'https://test2.com/;https://test3.com/';
-
-  it('returns false if the current url is an excluded url', () => {
-    window.location = { href: 'https://test0.com/' };
-    expect(isMatchedUrl(excludedUrls, includedUrls)).toBe(false);
-  });
-
-  it('returns false if the current url is not included when other urls are included', () => {
-    window.location = { href: 'https://test4.com/' };
-    expect(isMatchedUrl('', includedUrls)).toBe(false);
-  });
-
-  it('returns true if the current url is not excluded and included is empty', () => {
-    window.location = { href: 'https://test2.com/' };
-    expect(isMatchedUrl('', '')).toBe(true);
-  });
-
-  it('returns true if the current url is not excluded and is included', () => {
-    window.location = { href: 'https://test2.com/' };
-    expect(isMatchedUrl('', includedUrls)).toBe(true);
-  });
-});
-
-describe('bannerIsValid()', () => {
-  const bannerApiData = {
-    BannerId: 1,
-    Excluding: 'https://test0.com/;https://test1.com/',
-    Including: 'https://test2.com/;https://test3.com/',
-  };
-
-  it('returns false if the current url is an excluded url', () => {
-    window.location = { href: 'https://test0.com/' };
-    expect(bannerIsValid(bannerApiData)).toBe(false);
-  });
-
-  it('returns false if the current url is not included when other urls are included', () => {
-    window.location = { href: 'https://test4.com/' };
-    expect(bannerIsValid({
-      ...bannerApiData,
-      excludedUrls: '',
-    })).toBe(false);
-  });
-
-  it('returns true if the current url is not excluded and included is empty', () => {
-    window.location = { href: 'https://test2.com/' };
-    expect(bannerIsValid({
-      ...bannerApiData,
-      excludedUrls: '',
-      includedUrls: '',
-    })).toBe(true);
-  });
-
-  it('returns true if the current url is not excluded and is included', () => {
-    window.location = { href: 'https://test2.com/' };
-    expect(bannerIsValid({
-      ...bannerApiData,
-      excludedUrls: '',
-    })).toBe(true);
-  });
-
-  it('returns false if a cookie is set for the banner', () => {
-    window.location = { href: 'https://test2.com/' };
-    setCookie(bannerApiData.BannerId, 99);
-    expect(bannerIsValid(bannerApiData)).toBe(false);
-  });
-});
-
 describe('insertBanner()', () => {
   beforeEach(() => {
     document.body.innerHTML = `<html>
@@ -166,22 +45,14 @@ describe('insertBanner()', () => {
   });
 
   it('inserts an alert banner when passed alert banner data and template', () => {
-    insertBanner(alertBannerData[0], alertBannerMarkup);
+    insertBanner(alertBannerData[0]);
     const banner = document.getElementById('nhsuk-global-alert');
     expect(banner.outerHTML).toBe(alertBannerMarkup);
   });
 
   it('inserts the coronavirus banner when passed banner data and template', () => {
-    insertBanner(coronavirusBannerData[0], feedbackBannerMarkup);
+    insertBanner(coronavirusBannerData[0]);
     const banner = document.getElementById('nhsuk-global-alert');
     expect(banner.outerHTML).toBe(coronavirusBannerMarkup);
-  });
-
-  it('inserts a feedback banner when passed feedback banner data and template', () => {
-    jest.useFakeTimers();
-    insertBanner(feedbackBannerData[0], feedbackBannerMarkup);
-    jest.advanceTimersByTime(3000);
-    const banner = document.getElementById('nhsuk-feedback-banner');
-    expect(banner.outerHTML).toBe(feedbackBannerMarkup);
   });
 });
